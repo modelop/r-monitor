8a502ed5c830590d8f5100e36a415c7504f49de1
dtrier <david.trier@gmail.com>
2023-04-05 14:39:58 -0400

    Update performance.R

--- performance.R
+++ performance.R
@@ -13,7 +13,7 @@ metrics <- function(data) {
     df <- data.frame(data)
     get_metrics <- metric_set(f_meas, accuracy, sensitivity, specificity, precision)
     output <- get_metrics(data = df, truth = as.factor(label_value), estimate = as.factor(score))
-    mtr <- paste("PerformanceMetrics: ", toString(output))
+    mtr <- list(PerformanceMetrics=output)
     print(mtr)
     emit(toJSON(mtr))
 }
