593cc0b55f2cbf40e2351fc9fa5e18eec527371b
dtrier <david.trier@gmail.com>
2023-04-05 12:38:54 -0400

    Create performance-r.py

---
+++ performance-r.py
@@ -0,0 +1,17 @@
+# import libraries
+library(tidymodels)
+library(readr)
+
+# modelop.init
+begin <- function() {
+    # Need to add schema gen
+}
+
+
+# modelop.metrics
+metrics <- function(data) {
+    df <- data.frame(data)
+    get_metrics <- metric_set(f_meas, accuracy, sensitivity, specificity, precision)
+    output <- get_metrics(data = df, truth = as.factor(label_value), estimate = as.factor(score))
+    emit(output)
+}
